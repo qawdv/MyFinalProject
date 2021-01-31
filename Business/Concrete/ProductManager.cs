c0641419898c883f936040cc54d2047f0b4e4cf5
Ersin Sular <ersinsular1994@gmail.com>
2021-01-31 21:24:03 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            // iş kodları varsa
+            // yetkisi var mı?
+            // şöyle böyle kurallar varsa ve geçtiyse aşağısı çalışır
+            return _productDal.GetAll();
+
+        }
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal  // I interface- Product Entity- Dal dataaccesslayer
+    {
+        List<Product> GetAll(); // entities den buraya bağladık
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+        List<Product> GetAllCategory(int CategoryId);
+
+    }
+}
